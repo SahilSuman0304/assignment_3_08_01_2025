e03c9480de9c4238342ea8e2743494031cb93e2c
SahilSuman0304 <sahilsuman0304@gmail.com>
2025-01-08 17:30:52 +0530

    assignment_3

---
+++ Assignment_3/src/main/java/com/assignment_3/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.assignment_3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@GetMapping("/front_page")
+public String web_controller() {
+	return "Hello WOrld, Adil is dick";
+}
+}
